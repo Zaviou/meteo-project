be4b8413e6e55106129753f6f83331f2e8782cd4
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2022-12-30 00:09:09 +0100

    màj C
    globalement ce que je pense qu'il va se passer avec le C sur ce projet

--- partieCtab.c
+++ partieCtab.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "headeralex.h"
 
 /* Order used for argv's content :    Si le C filtre les lieux et dates, sinon il faut enlever 10,11,12
 0 : command's name
@@ -23,65 +21,84 @@
 int main(int argc, char* argv){
   
   
-  
     FILE* input = fopen(argv[1],'r');            // The input file given by shell is opened.
     if(input==NULL){                             // Error code 2 if there was a problem opening the file.  
         return 2;
     }
+  
     FILE* output = fopen(argv[2],'w');           // The output file given by shell is created or starts being rewritten.
     if(output==NULL){                            // Error code 3 if there was a problem with this file.
         return 3;
     }
   
   
-  
-    if(argv[13]==0){                              // The user did not activate descending sort.
-        if(argv[3]==1 || argv[5]==1){             // Executes if temperature or pressure was chosen (we put them in the same "if" because their outputs are very similar).
-            if(argv[4]==1 || argv[6]==1){         // Executes if mode 1 was chosen for temperature or pressure.
-                tabTPsort11(input);
+    if(argv[13]==0){                             // The user did not activate descending sort.
+        if(argv[3]==1){                          // Executes if temperature was chosen.
+            if(argv[4]==1){                      // Executes if mode 1 was chosen for temperature.
+                tabTsort11(input);
             }
-            else if(argv[4]==2 || argv[6]==2){    // Executes if mode 2 was chosen for temperature or pressure.
-                tabTPsort21(input);
+            else if(argv[4]==2){                 // Executes if mode 2 was chosen for temperature.
+                tabTsort21(input);
             }
-            else{                                 // Executes if mode 3 was chosen for temperature or pressure.
-                tabTPsort31(input);
+            else{                                // Executes if mode 3 was chosen for temperature.
+                tabTsort31(input);
             }
         }
-        else if(argv[7]==1){                      // Executes if wind was chosen.
+        else if(argv[5]==1){                     // Executes if pressure was chosen.
+            if(argv[6]==1){                      // Executes if mode 1 was chosen for pressure.
+                tabPsort11(input);
+            }
+            else if(argv[6]==2){                 // Executes if mode 2 was chosen for pressure.
+                tabPsort21(input);
+            }
+            else{                                // Executes if mode 3 was chosen for pressure.
+                tabPsort31(input);
+            }
+        }
+        else if(argv[7]==1){                     // Executes if wind was chosen.
             tabWsort1(input);
         }
-        else if(argv[8]==1){                      // Executes if height was chosen.
+        else if(argv[8]==1){                     // Executes if height was chosen.
             tabHsort1(input);
         }
-        else{                                     // Executes if moisture was chosen.
+        else{                                    // Executes if moisture was chosen.
             tabMsort1(input);
         }
     }
   
   
-    else{                                         // The user activated descending sort.
-        if(argv[3]==1 || argv[5]==1){         
-            if(argv[4]==1 || argv[6]==1){         // Executes if mode 1 was chosen for temperature or pressure.
-                tabTPsort12(input);
+    else{                                        // The user activated descending sort.
+        if(argv[3]==1){         
+            if(argv[4]==1){                      // Executes if mode 1 was chosen for temperature or pressure.
+                tabTsort12(input);
             }
-            else if(argv[4]==2 || argv[6]==2){    // Executes if mode 2 was chosen for temperature or pressure.
-                tabTPsort22(input);
+            else if(argv[4]==2){                 // Executes if mode 2 was chosen for temperature or pressure.
+                tabTsort22(input);
             }
-            else{                                 // Executes if mode 3 was chosen for temperature or pressure.
-                tabTPsort32(input);
+            else{                                // Executes if mode 3 was chosen for temperature or pressure.
+                tabTsort32(input);
             }
         }
-        else if(argv[7]==1){                      // Executes if wind was chosen.
+        else if(argv[5]==1){         
+            if(argv[6]==1){                      // Executes if mode 1 was chosen for temperature or pressure.
+                tabPsort12(input);
+            }
+            else if(argv[6]==2){                 // Executes if mode 2 was chosen for temperature or pressure.
+                tabPsort22(input);
+            }
+            else{                                // Executes if mode 3 was chosen for temperature or pressure.
+                tabPsort32(input);
+            }
+        }
+        else if(argv[7]==1){                     // Executes if wind was chosen.
             tabWsort2(input);
         }
-        else if(argv[8]==1){                      // Executes if height was chosen.
+        else if(argv[8]==1){                     // Executes if height was chosen.
             tabHsort2(input);
         }
-        else{                                     // Executes if moisture was chosen.
+        else{                                    // Executes if moisture was chosen.
             tabMsort2(input);
         }
     }
-  
-  
-    return 0;                                     // If no error occured by now, the program executed successfully and returns 0.
+    return 0;                                    // If no error occured by now, the program executed successfully and returns 0.
 }
