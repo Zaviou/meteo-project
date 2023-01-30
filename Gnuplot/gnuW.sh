8bf4aea674dc91cd08e07c6e0dde45305f4c28fe
Camoht <104215342+Camoht@users.noreply.github.com>
2023-01-30 17:58:29 +0100

    màj
    chemin relatif fichier entrée

--- Gnuplot/gnuW.sh
+++ Gnuplot/gnuW.sh
@@ -9,4 +9,4 @@ set title "Flêches graph W"
 set xlabel ' Coordonate X '
 set ylabel ' Coordonate Y '
 
-plot 'C/data/temp_W.csv' using 1:2:($3*cos($4/180*pi)):($3*sin($4/180*pi)) with vectors head filled lt 2 title "Direction of wind"
+plot 'data/temp_W.csv' using 1:2:($3*cos($4/180*pi)):($3*sin($4/180*pi)) with vectors head filled lt 2 title "Direction of wind"
