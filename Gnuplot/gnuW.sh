c6819677645ab326f3e23bebd444d27a2bffc2a8
Camoht <104215342+Camoht@users.noreply.github.com>
2023-01-30 18:13:12 +0100

    màj
    translate french into english

--- Gnuplot/gnuW.sh
+++ Gnuplot/gnuW.sh
@@ -5,8 +5,8 @@ set terminal png size 1800,1000
 set output "load_W"
 
 # Putting a legend.
-set title "Flêches graph W"
-set xlabel ' Coordonate X '
-set ylabel ' Coordonate Y '
+set title "Graph in vectors of wind"
+set xlabel ' Latitude '
+set ylabel ' Longitude '
 
 plot 'data/temp_W.csv' using 1:2:($3*cos($4/180*pi)):($3*sin($4/180*pi)) with vectors head filled lt 2 title "Direction of wind"
