54952bdd6b92de92f9b7e2e815e0bd348ac96ce3
Camoht <104215342+Camoht@users.noreply.github.com>
2023-01-30 18:07:55 +0100

    màj
    translate french into english

--- Gnuplot/gnuP3.sh
+++ Gnuplot/gnuP3.sh
@@ -5,9 +5,9 @@ set terminal png size 1800,1000
 set output "load_P3"
 
 # Putting a legend.
-set title "Multiples lignes graph P3"
-set xlabel ' Station '
-set ylabel ' Pression '
+set title "Graph of pressure versus time"
+set xlabel ' Date '
+set ylabel ' Pressure '
 
 #Settings the format of date.
 set xdata time
