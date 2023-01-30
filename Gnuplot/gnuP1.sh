1ebb8de4cd2d599f0a4bb8e5817baefce408647c
Camoht <104215342+Camoht@users.noreply.github.com>
2023-01-30 17:55:32 +0100

    màj
    chemin relatif fichier entrée

--- Gnuplot/gnuP1.sh
+++ Gnuplot/gnuP1.sh
@@ -9,5 +9,5 @@ set title "to complete"
 set xlabel ' Id sation '
 set ylabel ' Pressure '
 
-plot 'C/data/temp_T1.csv' using 1:3:2 with filledcurve title "Max and min pressure" lc rgb "orange",\
-	'C/data/temp_T1.csv' using 1:4 with linespoints title "Average pressure" lc rgb "green"
+plot 'data/temp_T1.csv' using 1:3:2 with filledcurve title "Max and min pressure" lc rgb "orange",\
+	'data/temp_T1.csv' using 1:4 with linespoints title "Average pressure" lc rgb "green"
