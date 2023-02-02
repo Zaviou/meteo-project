f1eb50c58e5ffeef8a87f1d6cd1fc997dde0dd06
Camoht <104215342+Camoht@users.noreply.github.com>
2023-02-02 09:29:47 +0100

    màj
    chemin fichier temp

--- Gnuplot/gnuW.sh
+++ Gnuplot/gnuW.sh
@@ -9,4 +9,4 @@ set title "Graph in vectors of wind"
 set xlabel ' Latitude '
 set ylabel ' Longitude '
 
-plot 'data/temp_W.csv' using 1:2:($3*cos($4/180*pi)):($3*sin($4/180*pi)) with vectors head filled lt 2 title "Direction of wind"
+plot 'Gnuplot/data/temp_W.csv' using 1:2:($3*cos($4/180*pi)):($3*sin($4/180*pi)) with vectors head filled lt 2 title "Direction of wind"
