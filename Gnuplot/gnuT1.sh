436cca71847e750d138f3672be03c3f1586cf4a8
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2023-02-02 18:01:54 +0100

    ppp

--- Gnuplot/gnuT1.sh
+++ Gnuplot/gnuT1.sh
@@ -9,5 +9,5 @@ set title "Temperature error bars"
 set xlabel ' Id sation'
 set ylabel ' Temperatures '
 
-plot 'Gnuplot/data/temp_T1.csv' using 1:3:2 with filledcurve title "Max and min temperature" lc rgb "orange",\
-	'Gnuplot/data/temp_T1.csv' using 1:4 with linespoints title "Average temperature" lc rgb "green"
+plot 'Gnuplot/data/temp_T.csv' using 1:3:2 with filledcurve title "Max and min temperature" lc rgb "orange",\
+	'Gnuplot/data/temp_T.csv' using 1:4 with linespoints title "Average temperature" lc rgb "green"
