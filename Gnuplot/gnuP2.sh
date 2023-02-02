82cccad2235cae43ddd2908d83ce51afea173999
alexandregrisez <104682287+alexandregrisez@users.noreply.github.com>
2023-02-02 17:45:13 +0100

    abscisse

--- Gnuplot/gnuP2.sh
+++ Gnuplot/gnuP2.sh
@@ -12,6 +12,6 @@ set ylabel ' Pressure '
 #Settings the format of date.
 set xdata time
 set timefmt '"%Y-%m-%d %H:%M:%S"'
-set format x "day %d\n%Hh"
+set format x "day %d/%m/%Y\n%Hh"
 
 plot 'Gnuplot/data/temp_P.csv' using 1:2 with linespoints title "Average pressure"
