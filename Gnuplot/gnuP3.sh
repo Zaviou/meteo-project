62ede9098652ae42404218ded2d8da6e0acef6a8
Camoht <104215342+Camoht@users.noreply.github.com>
2023-02-02 16:48:09 +0100

    màj
    erreur fichier départ

--- Gnuplot/gnuP3.sh
+++ Gnuplot/gnuP3.sh
@@ -14,4 +14,4 @@ set xdata time
 set timefmt '"%Y-%m-%d"'
 set format x "%d/%m/%Y"
 
-plot 'Gnuplot/data/temp_P3.csv' using 1:3:4 with linespoints lc palette z title "Hours"
+plot 'Gnuplot/data/temp_P.csv' using 1:3:4 with linespoints lc palette z title "Hours"
